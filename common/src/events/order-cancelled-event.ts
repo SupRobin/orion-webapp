efa1a3d19baed12d2f24961b131e7c16f57fb656
FlavioDuran1 <Flavio.duran@aetion.com>
2023-11-18 10:45:52 -0500

    Updates

---
+++ common/src/events/order-cancelled-event.ts
@@ -0,0 +1,12 @@
+import {Subjects} from "./subjects"
+
+//do you want to share the max or minimum. we may want to add the price of the thing.
+export interface OrderCancelledEvent {
+    subject: Subjects.OrderCancelled;
+    data: {
+        id: string;
+        ticket: {
+            id: string;
+        }
+    }
+}
