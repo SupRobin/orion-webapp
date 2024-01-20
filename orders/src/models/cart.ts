ed37c69e4bc911e5ff1e2dc40f429a2a8ef43a03
FlavioDuran1 <Flavio.duran@aetion.com>
2024-01-20 11:02:44 -0500

    updates and some backend configurations changes.

---
+++ orders/src/models/cart.ts
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+import {Order, OrderStatus} from "./order";
+import {updateIfCurrentPlugin} from "mongoose-update-if-current";
+
+interface CartAttrs {
+    cartId: string;
+    userId: string;
+}
+
+export interface CartDoc extends mongoose.Document {
+    cartId: string;
+    userId: string;
+    isReserved(): Promise<boolean>;
+}
+
+interface CartModel extends mongoose.Model<CartDoc> {
+    build(attrs: CartAttrs): CartDoc;
+    findByEvent(event: {
+        cartId: string;
+        userId: string | null;
+    }): Promise<CartDoc | null>;
+}
+
+const cartSchema = new mongoose.Schema(
+    {
+        cartId: {
+            type: String,
+            required: true,
+        },
+        userId: {
+            type: String,
+            required: false,
+        },
+    },
+    {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+            },
+        },
+    }
+);
+
+cartSchema.set('versionKey', 'version');
+cartSchema.plugin(updateIfCurrentPlugin);
+
+cartSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+    return Cart.findOne({
+        _id: event.id,
+        version: event.version - 1,
+    });
+};
+cartSchema.statics.build = (attrs: CartAttrs) => {
+    return new Cart({
+        _id: attrs.userId,
+        userId: attrs.userId,
+    });
+};
+cartSchema.methods.isReserved = async function () {
+    // this === the cart document that we just called 'isReserved' on
+    const existingOrder = await Order.findOne({
+        cart: this,
+        status: {
+            $in: [
+                OrderStatus.Created,
+                OrderStatus.AwaitingPayment,
+                OrderStatus.Complete,
+            ],
+        },
+    });
+
+    return !!existingOrder;
+};
+
+const Cart = mongoose.model<CartDoc, CartModel>('Cart', cartSchema);
+
+export { Cart };
