b24012790bde23e19e582754086feb72796725ed
FlavioDuran1 <flavio.m.duran@gmail.com>
2024-02-04 17:04:22 -0500

    random push

--- cart/src/events/publishers/cart-item-added-publisher.ts
+++ cart/src/events/publishers/cart-item-added-publisher.ts
@@ -3,4 +3,3 @@ import {CartItemAddedEvent, Subjects, Publisher} from "@orionco/common";
 export class CartItemAddedPublisher extends Publisher<CartItemAddedEvent> {
     readonly subject = Subjects.CartItemAdded;
 }
-
